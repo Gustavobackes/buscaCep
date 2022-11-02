e63ae5fd8d0118b6895b84b857b35882bf8aa2ae
gustavobackes <gustavo.kitzberger@projuris.com.br>
2022-11-02 20:52:57 -0300

    comecando a passar para .ts

---
+++ dist/js/index.js
@@ -0,0 +1,62 @@
+"use strict";
+let rua = document.querySelector("#rua");
+let bairro = document.querySelector("#bairro");
+let cidade = document.querySelector("#cidade");
+let uf = document.querySelector("#uf");
+let ibge = document.querySelector("#ibge");
+function limpa_formulario_cep() {
+    //Limpa valores do formulario de cep.
+    rua.innerHTML = "";
+    bairro.innerHTML = "";
+    cidade.innerHTML = "";
+    uf.innerHTML = "";
+    ibge.innerHTML = "";
+}
+function meu_callback(conteudo) {
+    if (!("erro" in conteudo)) {
+        //Atualiza os campos com os valores.
+        rua = conteudo.logradouro;
+        bairro = conteudo.bairro;
+        cidade = conteudo.localidade;
+        uf = conteudo.uf;
+        ibge = conteudo.ibge;
+    } //end if.
+    else {
+        //CEP nao Encontrado.
+        limpa_formulario_cep();
+        alert("CEP nao encontrado.");
+    }
+}
+function pesquisacep(valor) {
+    //Nova variavel "cep" somente com dígitos.
+    const cep = valor.replace(/\D/g, "");
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+        //Expressao regular para validar o CEP.
+        var validacep = /^[0-9]{8}$/;
+        //Valida o formato do CEP.
+        if (validacep.test(cep)) {
+            //Preenche os campos com "..." enquanto consulta webservice.
+            rua.innerHTML = "...";
+            bairro.innerHTML = "...";
+            cidade.innerHTML = "...";
+            uf.innerHTML = "...";
+            ibge.innerHTML = "...";
+            //Cria um elemento javascript.
+            var script = document.createElement("script");
+            //Sincroniza com o callback.
+            script.src = `https://viacep.com.br/ws/${cep}/json/?callback=meu_callback`;
+            //Insere script no documento e carrega o conteudo.
+            document.body.appendChild(script);
+        } //end if.
+        else {
+            //cep e invalido.
+            limpa_formulario_cep();
+            alert("Formato de CEP invalido.");
+        }
+    } //end if.
+    else {
+        //cep sem valor, limpa formulario.
+        limpa_formulario_cep();
+    }
+}
